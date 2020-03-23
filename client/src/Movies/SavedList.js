8c609e29fb02886e188c76d3bf0c1bf76bbc9ebe
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-03-23 17:08:54 -0600

    MVP

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SavedList = props => (
   <div className="saved-list">
@@ -7,7 +7,9 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <button className="home-button">Home</button>
+    <Link to={"/"} className="home-button">
+      Home
+    </Link>
   </div>
 );
 
